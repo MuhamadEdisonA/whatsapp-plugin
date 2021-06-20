bcad873c48b669ca0dfb4241e4e37a4b09403c9a
MuhamadEdisonA <68976611+MuhamadEdisonA@users.noreply.github.com>
2021-06-21 06:18:15 +0700

    Add files via upload

---
+++ whatsapp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * 
+ */
+/*
+Plugin Name: Whatsapp 
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: Whatsapp plugin untuk chat di wordpress website
+Author: Muhamad Edison A
+Version: 1.7.2
+Author URI: http://ma.tt/
+*/
+
+//mengambil data dari file fungsi.php
+include('fungsi.php');
+?>
